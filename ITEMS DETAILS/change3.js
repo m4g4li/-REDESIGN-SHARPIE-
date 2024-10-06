6b0ba391e2c0a7b19a20ce0484a899098b0cf076
M4G4LI <magali.berganza@catolica.edu.sv>
2024-10-06 10:28:17 -0600

    agregar seccion marcadores

---
+++ ITEMS DETAILS/change3.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 1
+    document.getElementById('black3').addEventListener('click', function() {
+        window.location.href = 'item3.html'; 
+    });
+
+    document.getElementById('red3').addEventListener('click', function() {
+        window.location.href = 'item3red.html'; 
+    });
+
+    document.getElementById('blue3').addEventListener('click', function() {
+        window.location.href = 'item3blue.html'; 
+    });
+
+    document.getElementById('green3').addEventListener('click', function() {
+        window.location.href = 'item3green.html'; 
+    });
+
+    document.getElementById('purple3').addEventListener('click', function() {
+        window.location.href = 'item3purple.html'; 
+    });
+});
---
+++ ITEMS DETAILS/change5.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 1
+    document.getElementById('black5').addEventListener('click', function() {
+        window.location.href = 'item5.html'; 
+    });
+
+    document.getElementById('colors5').addEventListener('click', function() {
+        window.location.href = 'item5colors.html'; 
+    });
+
+
+});
