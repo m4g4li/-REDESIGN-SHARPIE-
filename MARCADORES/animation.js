5e858db85dbc5300a43f1d11e735921dbf73e68e
M4G4LI <magali.berganza@catolica.edu.sv>
2024-11-03 11:22:25 -0600

    agregar animacion

---
+++ MARCADORES/animation.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal(".marcadores1 h2", {
+    duration: 1000,  // Duración de la animación
+    origin: "bottom",  // Animación desde la izquierda
+    distance: "50px",  // Distancia que se mueve el elemento
+    delay: 200,  // Retraso antes de la animación
+  });
+  
+  ScrollReveal().reveal(".marcadores1 p", {
+    duration: 1000,
+    origin: "bottom",  // Animación desde la derecha para variar
+    distance: "50px",
+    delay: 400,  // Retraso adicional para que el párrafo aparezca después del h3
+  });
