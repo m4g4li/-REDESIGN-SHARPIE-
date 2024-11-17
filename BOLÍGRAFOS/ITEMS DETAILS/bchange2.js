8bc053be12fdde4f321ec668bdcc852c809f24c3
M4G4LI <magali.berganza@catolica.edu.sv>
2024-11-17 10:42:12 -0600

    agregar seccion BOLÍGRAFOS

---
+++ BOLÍGRAFOS/ITEMS DETAILS/bchange2.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+   // ITEM 2
+    document.getElementById('black2').addEventListener('click', function() {
+        window.location.href = 'bitem2.html'; 
+    });
+
+    document.getElementById('red2').addEventListener('click', function() {
+        window.location.href = 'bitem2red.html'; 
+    });
+
+    document.getElementById('blue2').addEventListener('click', function() {
+        window.location.href = 'bitem2blue.html';
+    });
+});
---
+++ BOLÍGRAFOS/ITEMS DETAILS/bchange3.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ITEM 2
+     document.getElementById('black3').addEventListener('click', function() {
+         window.location.href = 'bitem3.html'; 
+     });
+ 
+     document.getElementById('red3').addEventListener('click', function() {
+         window.location.href = 'bitem3red.html'; 
+     });
+ 
+     document.getElementById('blue3').addEventListener('click', function() {
+         window.location.href = 'bitem3blue.html';
+     });
+ });
